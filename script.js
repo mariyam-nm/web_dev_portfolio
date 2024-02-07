ba3cb3caa35ecf7d2d602357f181ac5f286dd3f5
Mariyam Mohammed <mariyammohammed@Mariyams-MacBook-Pro.local>
2024-02-06 23:31:55 -0500

    Initial commit for portfolio page

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
